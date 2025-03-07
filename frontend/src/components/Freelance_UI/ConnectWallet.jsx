62caca8d12e141ec6da8a907b26960d428a125c1
Rubikcon <rubikconnexus@gmail.com>
2025-03-08 00:46:55 +0100

    Abstracted the userRole on the selectRole component

--- frontend/src/components/Freelance_UI/ConnectWallet.jsx
+++ frontend/src/components/Freelance_UI/ConnectWallet.jsx
@@ -1,48 +1,3 @@
-// import { useState, useEffect } from "react";
-// import { BrowserProvider } from "ethers";
-
-// const ConnectWallet = ({ setSigner }) => {
-//   const [account, setAccount] = useState(null);
-
-//   const connectWallet = async () => {
-//     if (window.ethereum) {
-//       try {
-//         const provider = new BrowserProvider(window.ethereum);
-//         const signer = await provider.getSigner();
-//         const accounts = await provider.send("eth_requestAccounts", []);
-//         setAccount(accounts[0]);
-//         setSigner(signer);
-//       } catch (error) {
-//         console.error("Wallet connection failed:", error);
-//       }
-//     } else {
-//       alert("MetaMask not installed!");
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (account) {
-//       console.log("Connected account:", account);
-//     }
-//   }, [account]);
-//   // Runs whenever `account` changes
-
-//   return (
-//     <div className="p-4 text-center">
-//       {account ? (
-//         <p className="text-green-600 font-bold">Connected: {account}</p>
-//       ) : (
-//         <button
-//           onClick={connectWallet}
-//           className="bg-blue-500 text-white p-2 rounded cursor-pointer"
-//         >
-//           Connect Wallet
-//         </button>
-//       )}
-//     </div>
-//   );
-// };
-
 // export default ConnectWallet;
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -60,6 +15,11 @@ const ConnectWallet = ({ setSigner }) => {
         const accounts = await provider.send("eth_requestAccounts", []);
         setAccount(accounts[0]);
         setSigner(signer);
+
+        // Set the local storage data for future process
+        // ✅ Store role and signature
+        localStorage.setItem("userRole", selectedRole);
+        localStorage.setItem("walletSignature", signature);
 
         // Redirect to role selection page
         navigate("/select-role", { state: { account: accounts[0] } });
